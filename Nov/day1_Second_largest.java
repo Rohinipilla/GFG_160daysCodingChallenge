18711e7f6a77d45bf2298582cab8d49fad55d3d0
PILLA ROHINI <138279046+Rohinipilla@users.noreply.github.com>
2024-11-17 23:01:19 +0530

    Update and rename 15Nov2024.doc to day1_Second_largest.java

---
+++ Nov/day1_Second_largest.java
@@ -0,0 +1,26 @@
+
+class Solution {
+	public int getSecondLargest(int[] arr) {
+			// Code Here
+			int max1 = Integer.MIN_VALUE;
+			int max2 = Integer.MIN_VALUE;
+	    for(int i=0;i<arr.length;i++){
+	
+	        if(arr[i]>max1){
+	
+	            max2 = max1;
+	            max1 = arr[i];
+	        }
+	        else if(arr[i]>max2 && arr[i]!=max1){
+	            max2 = arr[i];
+	        }
+	
+	    }
+	
+	    if(max1 == max2 || max2 == Integer.MIN_VALUE){
+	        return -1;
+	    }
+	
+	    return max2;
+	}
+}
